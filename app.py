a52012b3f92bb81fe11879ecfc46f9546330102e
barick <barick3018@gmail.com>
2025-08-05 12:35:03 +0530

    adding some changes

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def hello():
 return "🚀 Hello from Flask on EC2 deployed via Jenkins!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 app.run(host='0.0.0.0', port=5000)
 
